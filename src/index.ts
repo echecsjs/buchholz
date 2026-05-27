02b08dd1eb7528ca5f2a38ebd2bf30499fb44c33
Adrian de la Rosa <github@brick.email>
2026-05-27 20:22:02 +0200

    refactor: annotate tiebreak functions with Tiebreak type from tournament

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import { contributions } from './utilities.js';
 
-import type { CompletedRound, Player } from '@echecs/tournament';
+import type { Tiebreak } from '@echecs/tournament';
 
-function buchholz(
-  player: string,
-  rounds: CompletedRound[],
-  _players: Player[],
-): number {
-  return contributions(player, rounds).reduce((sum, c) => sum + c.value, 0);
-}
+const buchholz: Tiebreak = (player, rounds, _players) =>
+  contributions(player, rounds).reduce((sum, c) => sum + c.value, 0);
 
 export { buchholz, buchholz as tiebreak };
 
